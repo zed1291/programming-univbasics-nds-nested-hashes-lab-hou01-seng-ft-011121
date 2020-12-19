80a98b6573c45a9986f81807c5f0921fcc9751bb
zed <zed@kolk.me>
2020-12-19 05:34:06 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -35,7 +35,7 @@ def literal_aoh
 #     {:name=>"Pat Monteseian", :occupation=>"Staff"},
 #     {:name=>"Kate Gompert", :occupation=>"None"},
 #     {:name=>"Bruce Green", :occupation=>"Fan of Mildred"}]
-# end
+end
 
 def aoh_lookup(aoh, row, key)
   aoh[row][key]
